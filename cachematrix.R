d0ba8fd1d3161108f1c09998af069dc244570c86
mnikolik <marijanikolik@gmail.com>
2018-04-02 22:25:31 +0200

    finished assignment with comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # define the matrix
+        m <- NULL
+        # create a setter
+        set <- function(y){
+                # set the matrix to value y 
+                x <<- y
+                # reset cached inverse
+                m <<- NULL
+        }
+        # create a getter
+        get <- function() x
+        # setter for the inverse, set the value of m to provided parameter solve 
+        setSolve <- function(solve) m <<- solve
+        # getter for the inverse, returns the value of m
+        getSolve <- function() m
+        
+        # generate a list with all of the functions contained
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
@@ -12,4 +31,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        # call the "special matrix" and call function getSolve from it
+        # the value of getSolve can be NULL or a value of inverted matrix if one is cached
+        m <- x$getSolve()
+        # if the value is not null present a message on screen to show this is from cache and return the value of the matrix 
+        if(!is.null(m)) {
+                message("getting cached data")
+                # return value and stop execution of further commands
+                return(m)
+        }
+        # since the value of the cache is null, we got to this point
+        # get the matrix data
+        data <- x$get()
+        # solve the matrix
+        m <- Matrix::solve(data, ...)
+        # fill the cache with the solved value
+        x$setSolve(m)
+        # return the solved value
+        m
 }
